eabd82ac50379415036e0910a41a835e8c054982
Jahfer Husain <jahferh@gmail.com>
2014-01-25 11:20:01 -0500

    Remote testing folder

--- testing/js/stats.js
+++
@@ -1,7 +0,0 @@
-$ ->
-  stats = new Stats()
-  stats.setMode(0)
-  stats.domElement.style.position = 'absolute'
-  stats.domElement.style.left = '0px'
-  stats.domElement.style.right = '0px'
-  $('body').append(stats.domElement)
